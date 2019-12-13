9032c08ff98db0d46bb9fdf84c5d9cfe37ae4ad9
blisstoner <blisstoner@gmail.com>
2019-12-13 09:00:05 -0800

    python backend complete

--- cpp-backend/Connect6.h
+++ cpp-backend/Connect6.h
@@ -52,45 +52,45 @@ const double SCORE_FACTOR = 1.5;
 const int BOARD_SZ = 19;
 class Node {
 public:
-	int depth; // 현재 이후로 몇 수 앞까지 확인했는가?
-	int player; // 1 : 나, 2 : 상대
-	bool fullSearched; // depth까지 전부 search가 완료되었는가?
-	ull score[12]; // score[i] : i단계 들어갔을 때 최대 score
-														// player 관계 없이 현재 node의 player의 점수는 상위 32비트, opponent의 점수는 하위 32비트
-														// 0x3f3f3f3f3f3f3f3f가 INVALID임을 의미
-														// N단계 이후의 player 관점에서 점수를 대입시킴
-	MOVES opMove, myMove; // 직전 상대의 move, 이번 나의 move. pair에서 int로 전환할 경우 초기화시켜야 함
-	vector<Node*> children;
-	Node(MOVES _myMove, MOVES _opMove, int _player, ull _score);
-	~Node();
+  int depth; // 현재 이후로 몇 수 앞까지 확인했는가?
+  int player; // 1 : 나, 2 : 상대
+  bool fullSearched; // depth까지 전부 search가 완료되었는가?
+  ull score[12]; // score[i] : i단계 들어갔을 때 최대 score
+                            // player 관계 없이 현재 node의 player의 점수는 상위 32비트, opponent의 점수는 하위 32비트
+                            // 0x3f3f3f3f3f3f3f3f가 INVALID임을 의미
+                            // N단계 이후의 player 관점에서 점수를 대입시킴
+  MOVES opMove, myMove; // 직전 상대의 move, 이번 나의 move. pair에서 int로 전환할 경우 초기화시켜야 함
+  vector<Node*> children;
+  Node(MOVES _myMove, MOVES _opMove, int _player, ull _score);
+  ~Node();
 };
 class MinimaxTree {
 public:
-	Node * root;
-	int myBoard[BOARD_SZ][BOARD_SZ];
-	int limit;
-	clock_t st_time;
-	MinimaxTree();
-	void setMove(MOVES mv, int state);
-	void setPosition(POSITION pt, int state);
-	void moveRoot(MOVES _opMove);
-	bool expFullTree(Node* cur);
-	bool expTree();
-	bool expLeaf(Node* leaf); // leaf에서 한 단게 확장
-	bool incDepth(Node* cur); // depth 1단계 증가시킴
-	bool OOB(int x, int y); // (x,y)가 board 위에 있는지 판단
-	int threatCnt(MOVES opMoves, int player);
-	ull eval1(POSITION pt, int player);
-	ull eval2(MOVES myMove, int player);
-	ull modified_eval1(POSITION pt, int player);
-	MOVES Con6Move(MOVES myMove, int player);
-	bool IsNoThreat(MOVES myMove, int player);
-	POSITION Con6Begin1();
-	MOVES Con6Begin2();
-	int GetForcedMove(MOVES opMoves, int player, MOVES ForcedMove[]);
-	void GetSingleBestPosition(int player, POSITION BestPosition[], ull BestScore[]);
-	void GetBestMove(int player, MOVES BestMove[], ull BestScore[]);
-	MOVES getArbitraryMoves(int player);
+  Node * root;
+  int myBoard[BOARD_SZ][BOARD_SZ];
+  int limit;
+  clock_t st_time;
+  MinimaxTree();
+  void setMove(MOVES mv, int state);
+  void setPosition(POSITION pt, int state);
+  void moveRoot(MOVES _opMove);
+  bool expFullTree(Node* cur);
+  bool expTree();
+  bool expLeaf(Node* leaf); // leaf에서 한 단게 확장
+  bool incDepth(Node* cur); // depth 1단계 증가시킴
+  bool OOB(int x, int y); // (x,y)가 board 위에 있는지 판단
+  int threatCnt(MOVES opMoves, int player);
+  ull eval1(POSITION pt, int player);
+  ull eval2(MOVES myMove, int player);
+  ull modified_eval1(POSITION pt, int player);
+  MOVES Con6Move(MOVES myMove, int player);
+  bool IsNoThreat(MOVES myMove, int player);
+  POSITION Con6Begin1();
+  MOVES Con6Begin2();
+  int GetForcedMove(MOVES opMoves, int player, MOVES ForcedMove[]);
+  void GetSingleBestPosition(int player, POSITION BestPosition[], ull BestScore[]);
+  void GetBestMove(int player, MOVES BestMove[], ull BestScore[]);
+  MOVES getArbitraryMoves(int player);
 
 };
 
